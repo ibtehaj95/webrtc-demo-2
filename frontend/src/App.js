dacf17c70a20431d4b479b752c2bb85ef0748728
Ibtehaj Khan <muhammad.ibtehaj.khan@cml.fraunhofer.de>
2023-12-13 18:40:33 +0100

    cleanup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,26 +2,6 @@ import React, { useEffect, useState } from "react";
 
 function App (){
 
-    const [apiURL] = useState("http://127.0.0.1:3000/api/v1");  //backend api url
-
-    const hitServer = async () => {
-        const resp = await fetch(`${apiURL}/auth/login`, {
-            method: "POST",
-            // mode: "cors", // no-cors, *cors, same-origin
-            // credentials: "same-origin",
-            headers: {
-                "Content-Type": "application/json",
-                // 'Content-Type': 'application/x-www-form-urlencoded',
-              },
-            body: JSON.stringify({
-                "email": "alex@email.com",
-                "password": "secret",
-            }),
-        });
-        const respresp = await resp.json();
-        console.log(respresp);
-    }
-
     useEffect(() => {
         console.log("UseEffect");
         // hitServer();
