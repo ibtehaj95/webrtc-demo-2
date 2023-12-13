61e2ffa20f5111a85f2c6f954546632fb9f7b3ce
Ibtehaj Khan <muhammad.ibtehaj.khan@cml.fraunhofer.de>
2023-12-13 18:51:52 +0100

    cleanup again

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,6 @@ import React, { useEffect, useState } from "react";
 
 function App (){
 
-    useEffect(() => {
-        console.log("UseEffect");
-        // hitServer();
-    }, []);
-
     return(
         <div className="App">
             The is a Webpack React App
