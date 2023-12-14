c7dec7480ef8d57d14e79ea9b319d9df9fdc627a
Ibtehaj Khan <muhammad.ibtehaj.khan@cml.fraunhofer.de>
2023-12-14 12:19:33 +0100

    input stream working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,8 @@ function App (){
     const [callInputVal, setCallInputVal] = useState("");
 
     const handleStartWebcam = () => {
-        // TODO: Implement start webcam logic
+
+        getUserMedia(); // get the user's webcam/microphone stream
     };
 
     const handleCallRemote = () => {
@@ -22,6 +23,32 @@ function App (){
 
     const handleHangUp = () => {
         // TODO: Implement hang up logic
+    };
+
+    const userAllMediaDevices = () => {
+        navigator.mediaDevices.enumerateDevices()
+            .then(devices => {
+                devices.forEach(device => {
+                    console.log(device.kind.toUpperCase(), device.label);
+                })
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    };
+
+    const getUserMedia = () => {
+        const constraints = { video: true, audio: false }; // specify what kind of streams we want
+        // prompt the user for permission to use specified devices
+        // note that if a user has multiple cameras/microphones, we can find those out (enumerate) specify which ones to use, otherwise OS default will be used
+        // https://developer.mozilla.org/en-US/docs/Web/API/Media_Capture_and_Streams_API
+        navigator.mediaDevices.getUserMedia(constraints)
+            .then(stream => {
+                localVideoRef.current.srcObject = stream; // set the source of the video element to the captured stream
+            })
+            .catch(error => {
+                console.error("Error accessing media devices.", error);
+            });
     };
 
     return(
@@ -48,19 +75,22 @@ function App (){
                 </div>
             </div>
             <div className="inputs-container">
-                <Button variant="contained" color="primary" onClick={handleStartWebcam} sx={{margin: 1, width: "min-content"}}>
+                <Button variant="contained" color="primary" onClick={userAllMediaDevices} sx={{margin: 1, width: "max-content"}}>
+                    Show All Media Devices
+                </Button>
+                <Button variant="contained" color="primary" onClick={handleStartWebcam} sx={{margin: 1, width: "max-content"}}>
                     Start Webcam
                 </Button>
-                <Button variant="contained" color="primary" onClick={handleCallRemote} sx={{margin: 1, width: "min-content"}}>
+                <Button variant="contained" color="primary" onClick={handleCallRemote} sx={{margin: 1, width: "max-content"}}>
                     Call Remote
                 </Button>
                 <div>
-                    <TextField label="Call Input" variant="outlined" value={callInputVal} onChange={(event) => {setCallInputVal(event.target.value)}} sx={{margin: 1}}/>
-                    <Button variant="contained" color="primary" onClick={handleAcceptCall} sx={{margin: 1, width: "min-content"}}>
+                    <TextField label="Call Input" variant="outlined" size="small" value={callInputVal} onChange={(event) => {setCallInputVal(event.target.value)}} sx={{margin: 1}}/>
+                    <Button variant="contained" color="primary" onClick={handleAcceptCall} sx={{margin: 1, width: "max-content"}}>
                         Accept Call
                     </Button>
                 </div>
-                <Button variant="contained" color="primary" onClick={handleHangUp} sx={{margin: 1, width: "min-content"}}>
+                <Button variant="contained" color="primary" onClick={handleHangUp} sx={{margin: 1, width: "max-content"}}>
                     Hang Up
                 </Button>
             </div>
